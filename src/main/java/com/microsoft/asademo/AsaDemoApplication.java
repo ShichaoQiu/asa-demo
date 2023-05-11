1f1381464b004c3651452a1d4aeb268273cd1430
Shichao Qiu <rumengsichen@163.com>
2023-05-11 14:48:50 +0800

    init project

---
+++ src/main/java/com/microsoft/asademo/AsaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.asademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microsoft/asademo/AsaDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.microsoft.asademo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

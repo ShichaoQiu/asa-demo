d546e97759a66290c6d347490a8315fdb96abcf9
Shichao Qiu <rumengsichen@163.com>
2023-05-12 13:26:45 +0800

    refine the project

---
+++ src/main/java/com/microsoft/asademo/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.microsoft.asademo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @Value("${fileName:xxx}")
+    private String fileName;
+
+    @GetMapping("printLogs")
+    public String printLogs(){
+        int totalLines = 10;
+        for (int i = 0; i < totalLines; i++) {
+            log.info("asa demo test logs --- {}", i);
+        }
+        return String.format("Will print %d lines logs", totalLines);
+    }
+
+    @GetMapping("testConfig")
+    public String testConfig(){
+        return fileName;
+    }
+}

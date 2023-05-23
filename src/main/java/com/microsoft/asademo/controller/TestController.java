48943e9726d7a8bf5d05c0219960971b8095da8c
Shichao Qiu <rumengsichen@163.com>
2023-05-23 16:25:11 +0800

    refine the logic

--- src/main/java/com/microsoft/asademo/controller/TestController.java
+++ src/main/java/com/microsoft/asademo/controller/TestController.java
@@ -2,7 +2,9 @@ package com.microsoft.asademo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,15 @@ public class TestController {
     @Value("${fileName:xxx}")
     private String fileName;
 
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
+    @Autowired
+    private Environment environment;
+
     @GetMapping("printLogs")
     public String printLogs(){
-        int totalLines = 100000;
+        int totalLines = 100;
         for (int i = 0; i < totalLines; i++) {
             log.info("asa demo test logs --- {}", i);
         }
@@ -25,5 +33,11 @@ public class TestController {
     @GetMapping("testConfig")
     public String testConfig(){
         return fileName;
+    }
+
+    @GetMapping("testEnv")
+    public String[] testEnv(){
+        log.info("spring.profiles.active --- " + activeProfile);
+        return environment.getActiveProfiles();
     }
 }
